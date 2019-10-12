c0261b55f01b787dd5aafc6ec062160509539777
Arthur Chiu <hallo@achiurizo.co>
2019-10-12 05:39:58 +0000

    initial commit

---
+++ lib/ex_plasma.ex
@@ -0,0 +1,18 @@
+defmodule ExPlasma do
+  @moduledoc """
+  Documentation for ExPlasma.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExPlasma.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_plasma_test.exs
@@ -0,0 +1,8 @@
+defmodule ExPlasmaTest do
+  use ExUnit.Case
+  doctest ExPlasma
+
+  test "greets the world" do
+    assert ExPlasma.hello() == :world
+  end
+end
