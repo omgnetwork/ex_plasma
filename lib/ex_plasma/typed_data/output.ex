5fdcbae26424d2ffa7cbf9efb06ffd732577ce7d
achiurizo <24772+achiurizo@users.noreply.github.com>
2020-04-10 16:27:53 -0700

    add an empty output, typed_data hash for output

--- lib/ex_plasma/typed_data/output.ex
+++ lib/ex_plasma/typed_data/output.ex
@@ -7,11 +7,13 @@ defimpl ExPlasma.TypedData, for: ExPlasma.Output do
   @output_signature "Output(uint256 outputType,bytes20 outputGuard,address currency,uint256 amount)"
   @input_signature "Input(uint256 blknum,uint256 txindex,uint256 oindex)"
 
-  @doc """
-  """
   @spec encode(Output.t(), [as: atom()]) :: list()
   def encode(output, as: :input), do: do_encode_id(output.output_id)
   def encode(output, as: :output), do: do_encode(output)
+
+
+  @spec hash(Output.t(), [as: atom()]) :: binary()
+  def hash(output, options), do: output |> encode(options) |> do_hash(options)
 
   defp do_encode(%{output_type: type, output_data: data}) do
     [
@@ -30,5 +32,13 @@ defimpl ExPlasma.TypedData, for: ExPlasma.Output do
       encode_raw([txindex], [{:uint, 256}]),
       encode_raw([oindex], [{:uint, 256}])
     ]
+  end
+
+  defp do_hash([signature | encoded_list], _options) do
+    data = [Encoding.keccak_hash(signature) | encoded_list]
+
+    data
+    |> Enum.join()
+    |> Encoding.keccak_hash()
   end
 end
--- test/ex_plasma/typed_data/output_test.exs
+++ test/ex_plasma/typed_data/output_test.exs
@@ -1,32 +1,84 @@
 defmodule ExPlasma.TypedData.OutputTest do
-  #@moduledoc false
-  #use ExUnit.Case, async: true
+  @moduledoc false
+  use ExUnit.Case, async: true
 
-  #alias ExPlasma.TypedData
+  alias ExPlasma.TypedData
 
-  #describe "encode/2" do
-    #test "builds an EIP712 encodable output" do
-      #output = %{
-        #output_type: 1, 
-        #output_data: %{
-          #output_guard: <<0::160>>,
-          #token: <<0::160>>,
-          #amount: <<1>>
-        #}
-      #}
+  describe "encode/2" do
+    test "builds an EIP712 encodable output" do
+      output = %ExPlasma.Output{
+        output_type: <<1>>, 
+        output_data: %{
+          output_guard: <<0::160>>,
+          token: <<0::160>>,
+          amount: <<10>>
+        }
+      }
 
-      #ExP
-    #end
+      encoded = TypedData.encode(output, as: :output)
 
-    #test "builds an EIP712 encodable input" do
-    #end
-  #end
+      assert encoded ==
+               [
+                 "Output(uint256 outputType,bytes20 outputGuard,address currency,uint256 amount)",
+                 <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 1>>,
+                 <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0>>,
+                 <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0>>,
+                 <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 10>>
+               ]
+    end
 
-  #describe "hash/2" do
-    #test "hashes for an output" do
-    #end
+    test "builds an EIP712 encodable input" do
+      output = %ExPlasma.Output{
+        output_id: %{blknum: 1000, txindex: 0, oindex: 0}
+      }
 
-    #test "hashes for an output identifier" do
-    #end
-  #end
+      encoded = ExPlasma.TypedData.encode(output, as: :input)
+
+      assert encoded ==
+               [
+                 "Input(uint256 blknum,uint256 txindex,uint256 oindex)",
+                 <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 3, 232>>,
+                 <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0>>,
+                 <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0>>
+               ]
+    end
+  end
+
+  describe "hash/2" do
+    test "hashes an eip712 encoded output identifier" do
+      output = %ExPlasma.Output{
+        output_id: %{blknum: 0, txindex: 0, oindex: 0}
+      }
+
+      hashed = ExPlasma.TypedData.hash(output, as: :input)
+
+      assert hashed ==
+               <<26, 89, 51, 235, 11, 50, 35, 176, 80, 15, 187, 231, 3, 156, 171, 155, 173, 192,
+                 6, 173, 218, 108, 243, 211, 55, 117, 20, 18, 253, 122, 75, 97>>
+    end
+
+    test "hashes an eip712 encoded output" do
+      output = %ExPlasma.Output{
+        output_type: <<1>>, 
+        output_data: %{
+          output_guard: <<0::160>>,
+          token: <<0::160>>,
+          amount: <<10>>
+        }
+      }
+
+      hashed = ExPlasma.TypedData.hash(output, as: :output)
+
+      assert hashed ==
+               <<215, 8, 60, 19, 55, 10, 155, 112, 243, 199, 49, 150, 131, 140, 14, 12, 157, 118,
+                 195, 214, 198, 94, 223, 77, 159, 186, 45, 211, 125, 37, 234, 32>>
+    end
+  end
 end
