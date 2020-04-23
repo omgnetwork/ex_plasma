8e0ee88c674c3a92abe16732d70960596a41ca7b
Arthur Chiu <24772+achiurizo@users.noreply.github.com>
2020-04-23 12:01:00 -0700

    Update mix.exs
    Co-Authored-By: Ino Murko <ino.murko@outlook.com>

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule ExPlasma.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.3", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ethereumex, "~> 0.6.0", only: [:test]},
       {:ex_abi, "~> 0.2.0"},
