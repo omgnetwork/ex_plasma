cf086553965d91f41dbe408d3e6d128be4f173c7
Ayrat Badykov <ayratin555@gmail.com>
2020-07-22 12:47:41 +0300

    add Block.new test

--- test/ex_plasma/block_test.exs
+++ test/ex_plasma/block_test.exs
@@ -1,4 +1,23 @@
 defmodule ExPlasma.BlockTest do
   use ExUnit.Case, async: true
   doctest ExPlasma.Block
+
+  alias ExPlasma.Block
+
+  describe "new/1" do
+    test "creates a new block" do
+      transaction = %ExPlasma.Transaction{
+        inputs: [],
+        metadata: <<0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0>>,
+        sigs: [],
+        tx_data: 0,
+        tx_type: 1
+      }
+
+      result = Block.new([transaction])
+
+      assert result.transactions == [transaction]
+      assert result.hash == ExPlasma.Encoding.merkle_root_hash([ExPlasma.Transaction.encode(transaction)])
+    end
+  end
 end
