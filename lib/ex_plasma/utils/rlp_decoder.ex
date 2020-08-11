a370fb5b51ce77cb70eff7313efde1c72dafe552
Mederic <mederic@omise.co>
2020-08-11 20:51:09 +0700

    add missing error type in rlp_decoder

--- lib/ex_plasma/utils/rlp_decoder.ex
+++ lib/ex_plasma/utils/rlp_decoder.ex
@@ -3,6 +3,8 @@ defmodule ExPlasma.Utils.RlpDecoder do
   Provides functions to decode various data types from RLP raw format
   """
 
+  @type parse_uint256_errors() :: :leading_zeros_in_encoded_uint | :encoded_uint_too_big | :malformed_uint256
+
   @doc """
   Attempt to decode the given binary to a list of RLP items
   """
@@ -16,8 +18,7 @@ defmodule ExPlasma.Utils.RlpDecoder do
   @doc """
   Parses unsigned at-most 32-bytes integer. Leading zeros are disallowed
   """
-  @spec parse_uint256(binary()) ::
-          {:ok, non_neg_integer()} | {:error, :encoded_uint_too_big | :leading_zeros_in_encoded_uint}
+  @spec parse_uint256(binary()) :: {:ok, non_neg_integer()} | {:error, parse_uint256_errors()}
   def parse_uint256(<<0>>), do: {:ok, 0}
   def parse_uint256(<<0>> <> _binary), do: {:error, :leading_zeros_in_encoded_uint}
   def parse_uint256(binary) when byte_size(binary) <= 32, do: {:ok, :binary.decode_unsigned(binary, :big)}
